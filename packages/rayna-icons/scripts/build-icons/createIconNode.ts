41ebd7dc118915f113da47302a6e45bc0076b95a
Victor Adewusi <glove.adewusi@gmail.com>
2024-02-11 11:06:45 +0000

    feat: create icon nodes containing icon metadata

---
+++ packages/rayna-icons/scripts/build-icons/createIconNode.ts
@@ -0,0 +1,66 @@
+/* eslint-disable import/no-relative-packages */
+import { load } from "cheerio";
+import keywordJSON from "../../../../keywords.json" assert { type: "json" };
+import { IconNode } from "../../src/types";
+import { iconNameToPascalCase } from "./helper";
+import { optimizeSVG } from "./optimizeSVG";
+
+const keywords = keywordJSON as Record<string, string[]>;
+
+export const createIconNode = (
+  svg: ReturnType<typeof optimizeSVG>[number],
+): IconNode => {
+  const SVGElement = load(svg.content)("svg");
+  const SVGWidth = parseInt(SVGElement.attr("width") || "", 10);
+  const SVGHeight = parseInt(SVGElement.attr("height") || "", 10);
+  const SVGViewBox = SVGElement.attr("viewBox") || "";
+  const SVGPath = SVGElement.html() ?? "";
+  const SVGName = `${svg.fileName}-${svg.type}`;
+
+  if (!SVGHeight) {
+    throw new Error(`${SVGName}: Missing height attribute.`);
+  }
+
+  if (!SVGWidth) {
+    throw new Error(`${SVGName}: Missing width attribute.`);
+  }
+
+  if (!SVGViewBox) {
+    throw new Error(`${SVGName}: Missing viewBox attribute.`);
+  }
+
+  const viewBoxPattern = /0 0 ([0-9]+) ([0-9]+)/;
+
+  const viewBoxMatches = SVGViewBox.match(viewBoxPattern);
+
+  if (!viewBoxMatches) {
+    throw new Error(
+      `${SVGName}: Invalid viewBox attribute. Ensure the viewBox is in the following format: "0 0 <width> <height>"`,
+    );
+  }
+
+  const [, viewBoxWidth, viewBoxHeight] = viewBoxMatches;
+
+  if (SVGWidth !== parseInt(viewBoxWidth, 10)) {
+    throw new Error(
+      `${SVGName}: Invalid width attribute. The width attribute and the viewBox width do not match`,
+    );
+  }
+
+  if (SVGHeight !== parseInt(viewBoxHeight, 10)) {
+    throw new Error(
+      `${SVGName}: Invalid height attribute. The height attribute and the viewBox height do not match`,
+    );
+  }
+
+  return {
+    name: SVGName,
+    componentName: iconNameToPascalCase(SVGName),
+    type: svg.type,
+    width: SVGWidth,
+    height: SVGHeight,
+    viewBox: SVGViewBox,
+    keywords: keywords[svg.fileName],
+    path: SVGPath,
+  };
+};
--- packages/rayna-icons/scripts/build-icons/helper.ts
+++ packages/rayna-icons/scripts/build-icons/helper.ts
@@ -20,3 +20,11 @@ export const getArguments = () => {
     rawIconDirectory: argv.rawIconDirectory,
   };
 };
+
+/** Convert icon name (abc-def-ghi) to pascal case (AbcDefGhi) */
+export const iconNameToPascalCase = (name: string) => {
+  const parts = name.split("-");
+  return parts
+    .map((part) => `${part[0].toUpperCase()}${part.slice(1).toLowerCase()}`)
+    .join("");
+};
