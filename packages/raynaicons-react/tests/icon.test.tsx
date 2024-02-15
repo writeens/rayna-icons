0fd75374a8641d21ad2f3d36d0a5f09cd0f278e9
Victor Adewusi <glove.adewusi@gmail.com>
2024-02-15 08:53:15 +0000

    test(raynaicons-react): update tests to check for title

--- packages/raynaicons-react/tests/icon.test.tsx
+++ packages/raynaicons-react/tests/icon.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { it, describe, expect } from "vitest";
 // Build the iconfiles first to remove squiggly lines
-import { FirstAidFilled } from "../src/__generated__";
+import { FirstAidFilled, FirstAidOutlined } from "../src/__generated__";
 
 describe("FirstAidFilled", () => {
   it("is a valid react component", () => {
@@ -19,10 +19,52 @@ describe("FirstAidFilled", () => {
 
     const svg = getByTestId(testId)
 
-    screen.debug(svg)
-
     expect(svg).toHaveAttribute("class", "rayna rayna-first-aid-filled custom-class")
     expect(svg).toHaveAttribute("width", "40")
     expect(svg).toHaveAttribute("height", "40")
+  })
+
+  it("should generate a title tag within the SVG if a title prop is passed", () => {
+
+    const testId = "icon"
+
+    const { getByTestId } = render(<FirstAidFilled data-testid={testId} title="Hello World" />)
+
+    const svg = getByTestId(testId)
+
+    expect(svg.childNodes[0]).toHaveTextContent("Hello World")
+  })
+});
+
+describe("FirstAidOutlined", () => {
+  it("is a valid react component", () => {
+
+    const { container } = render(<FirstAidOutlined />)
+
+    expect(container).toMatchSnapshot()
+  });
+
+  it("should pass options correctly to the SVG", () => {
+
+    const testId = "icon"
+
+    const { getByTestId } = render(<FirstAidOutlined data-testid={testId} size={40} className="custom-class" />)
+
+    const svg = getByTestId(testId)
+
+    expect(svg).toHaveAttribute("class", "rayna rayna-first-aid-outlined custom-class")
+    expect(svg).toHaveAttribute("width", "40")
+    expect(svg).toHaveAttribute("height", "40")
+  })
+
+  it("should generate a title tag within the SVG if a title prop is passed", () => {
+
+    const testId = "icon"
+
+    const { getByTestId } = render(<FirstAidFilled data-testid={testId} title="Hello World" />)
+
+    const svg = getByTestId(testId)
+
+    expect(svg.childNodes[0]).toHaveTextContent("Hello World")
   })
 });
